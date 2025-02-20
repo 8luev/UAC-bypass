a012e3bb6239cc98e3d2f4f2be02e1c0729f2702
8luev <122008140+8luev@users.noreply.github.com>
2025-02-21 07:57:33 +1300

    added the manual method in a note

--- uac.bat
+++ uac.bat
@@ -1,1 +1,3 @@
 cmd /min /C "set __COMPAT_LAYER=runasinvoker && start "" "%1"
+
+:: To do the manual version make the run the commands "set __COMPAT_LAYER=runasinvoker" in a cmd window, then run "start your_app_name"
